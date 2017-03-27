0916cd89d587fe72effb3bb1c486d4b51ff65c0b
Jordi Polo <mumismo@gmail.com>
2017-03-27 11:08:21 +0900

    More proper handling of current dir and requests without filenames

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
 extern crate hyper;
+extern crate regex;
 
 use hyper::server::{Server, Request, Response};
 use hyper::uri::RequestUri;
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
+use std::env;
 
-//const ALLOWED_FILE_TYPES: [&str] = ["html", "js", "css"];
+use regex::Regex;
+
+fn to_full_filename(filename: &str) -> String {
+    let mut path = env::current_dir().unwrap();
+    path.push(filename);
+    path.into_os_string().into_string().unwrap()
+}
 
 fn parse_file_name(uri: &RequestUri) -> Option<String> {
-    let filename = uri.to_string();
-    let clean_filename = filename.split("?").nth(0).unwrap();
-    let mut fullname = ".".to_string();
-    fullname.push_str(clean_filename);
-    println!("Opening {}", fullname);
-    Some(fullname)
+    let extracts_filename = Regex::new(r"([\w\-\.]+[^#?\s]+).*?").unwrap();
+    extracts_filename.captures(&uri.to_string()).map(|captures| {
+        let full_filename = to_full_filename(&captures[1]);
+        println!("Attempting {:?}", full_filename);
+        full_filename
+    })
 }
 
 fn read_file(filename: &str) -> Result<String, io::Error> {
@@ -28,13 +36,21 @@ fn read_file(filename: &str) -> Result<String, io::Error> {
 fn handle_request(req: Request, res: Response) {
     match req.method {
         hyper::Get => {
-            match parse_file_name(&req.uri).map(|file_name| read_file(&file_name)).unwrap() {
-                Ok(file) => res.send(file.as_bytes()).unwrap(),
-                Err(error) => res.send(error.to_string().as_bytes()).unwrap(),
+            match parse_file_name(&req.uri) {
+                Some(filename) => {
+                    match read_file(&filename) {
+                        Ok(file) => res.send(file.as_bytes()).unwrap(),
+                        Err(error) => res.send(error.to_string().as_bytes()).unwrap(),
+                    }
+                },
+                None => res.send("For security reasons directories are not listed. Add a filename to the URL.".as_bytes()).unwrap(),
             }
+
         }
         _ => {
-            println!("Not supported"); //*res.status_mut() = StatusCode::MethodNotAllowed
+            println!("Not supported");
+            // TODO: how to modify status?
+            //*res.status_mut() = StatusCode::MethodNotAllowed
             res.send("Operation not supported".as_bytes()).unwrap();
         }
     }
