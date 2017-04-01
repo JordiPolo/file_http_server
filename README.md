3114eb9ccd86e147b15bfcaf995d20e85c7dc099
Jordi Polo <mumismo@gmail.com>
2017-04-01 15:39:16 +0900

    Deal correctly with common filetypes and base on latest hyper

---
+++ README.md
@@ -0,0 +1,5 @@
+
+Small http server serving local files without dependencies.
+
+Perfect when you want to show an static html site and do not want to
+have dependencies in the webserver.
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,120 @@
 extern crate hyper;
+extern crate futures;
 extern crate regex;
 
-use hyper::server::{Server, Request, Response};
-use hyper::uri::RequestUri;
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 use std::env;
-
+use std::path::PathBuf;
 use regex::Regex;
+use futures::future::FutureResult;
 
-fn to_full_filename(filename: &str) -> String {
+use hyper::server::{Http, Service, Request, Response};
+use hyper::{Get, StatusCode};
+use hyper::header::ContentLength;
+use hyper::header::ContentType;
+use hyper::mime;
+use hyper::mime::{Mime, TopLevel, SubLevel};
+
+
+fn to_full_filename(filename: &str) -> PathBuf {
     let mut path = env::current_dir().unwrap();
     path.push(filename);
-    path.into_os_string().into_string().unwrap()
+    path
 }
 
-fn parse_file_name(uri: &RequestUri) -> Option<String> {
+fn parse_file_name(uri: &str) -> Option<PathBuf> {
     let extracts_filename = Regex::new(r"([\w\-\.]+[^#?\s]+).*?").unwrap();
-    extracts_filename.captures(&uri.to_string()).map(|captures| {
+    extracts_filename.captures(uri).map(|captures| {
         let full_filename = to_full_filename(&captures[1]);
         println!("Attempting {:?}", full_filename);
         full_filename
     })
 }
 
-fn read_file(filename: &str) -> Result<String, io::Error> {
+fn filename_content_type(filename: &PathBuf) -> hyper::header::ContentType {
+    match filename.extension() {
+        Some(extension) => {
+            match extension.to_str().unwrap() {
+                "html" => ContentType::html(),
+                "css"  => ContentType(Mime(TopLevel::Text, SubLevel::Css, vec![])),
+                "js"   => ContentType(Mime(TopLevel::Application, SubLevel::Javascript, vec![])),
+                "jpg" | "jpeg" => ContentType::jpeg(),
+                "png" => ContentType::png(),
+                 _ => ContentType::plaintext()
+            }
+        },
+        None => ContentType::plaintext()
+
+    }
+}
+
+
+fn read_file(filename: &PathBuf) -> Result<Vec<u8>, io::Error> {
     let mut f = File::open(filename)?;
-    let mut buffer = String::new();
-    f.read_to_string(&mut buffer)?;
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer)?;
     Ok(buffer)
 }
 
 
-fn handle_request(req: Request, res: Response) {
-    match req.method {
-        hyper::Get => {
-            match parse_file_name(&req.uri) {
-                Some(filename) => {
-                    match read_file(&filename) {
-                        Ok(file) => res.send(file.as_bytes()).unwrap(),
-                        Err(error) => res.send(error.to_string().as_bytes()).unwrap(),
-                    }
-                },
-                None => res.send("For security reasons directories are not listed. Add a filename to the URL.".as_bytes()).unwrap(),
-            }
+struct FileServer;
 
+static INDEX: &'static [u8] = b"For security reasons directories are not listed. Add a filename to the URL.";
+static UNIMPLEMENTED: &'static [u8] = b"Operation not implemented";
+
+fn serve_file(url: &str) -> Response {
+    let filename = parse_file_name(url).unwrap();
+    match read_file(&filename) {
+        Ok(data) => {
+            let length = data.len();
+            Response::new()
+                .with_body(data)
+                .with_header(ContentLength(length as u64))
+                .with_header(filename_content_type(&filename))
+        },
+        Err(error) => {
+            println!("{:?}", error);
+            Response::new().with_status(StatusCode::NotFound)
         }
-        _ => {
-            println!("Not supported");
-            // TODO: how to modify status?
-            //*res.status_mut() = StatusCode::MethodNotAllowed
-            res.send("Operation not supported".as_bytes()).unwrap();
-        }
+
     }
 }
 
+
+impl Service for FileServer {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = FutureResult<Response, hyper::Error>;
+
+    fn call(&self, req: Request) -> Self::Future {
+        futures::future::ok(match (req.method(), req.path()) {
+            (&Get, "/") => {
+                Response::new()
+                    .with_header(ContentLength(INDEX.len() as u64))
+                    .with_body(INDEX)
+            },
+            (&Get, url) => {
+                serve_file(url)
+            },
+            (operation, url) => {
+                println!("Operation {:?} on {:?} not implemented", &operation, &url);
+                Response::new()
+                    .with_header(ContentLength(UNIMPLEMENTED.len() as u64))
+                    .with_body(UNIMPLEMENTED)
+            },
+        })
+    }
+}
+
+
+
 fn main() {
-    Server::http("127.0.0.1:8888").unwrap().handle(handle_request).unwrap();
+    let addr = "127.0.0.1:8888".parse().unwrap();
+
+    let server = Http::new().bind(&addr, || Ok(FileServer)).unwrap();
+    println!("Listening on http://{} with 1 thread.", server.local_addr().unwrap());
+    server.run().unwrap();
 }
